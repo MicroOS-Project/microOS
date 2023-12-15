8c9417bc6e83dc9d3282233189aa0ff55218d93e
Nathanael Asher <149738626+asherevan@users.noreply.github.com>
2023-12-15 15:09:53 -0700

    Update st7789py.py
    added the text() function from: https://github.com/adafruit/micropython-adafruit-rgb-display/blob/master/rgb_text.py

--- st7789py.py
+++ st7789py.py
@@ -1,6 +1,7 @@
 import time
 from micropython import const
 import ustruct as struct
+import framebuf
 
 # commands
 ST77XX_NOP = const(0x00)
@@ -62,6 +63,20 @@ _DECODE_PIXEL = ">BBB"
 
 _BUFFER_SIZE = const(256)
 
+def text(display, text, x=0, y=0, color=0xffff, background=0x0000):
+    x = min(display.width - 1, max(0, x))
+    y = min(display.height - 1, max(0, y))
+    w = display.width - x
+    h = min(display.height - y, 8)
+    buffer = bytearray(display.width * h * 2)
+    fb = framebuf.FrameBuffer(buffer, w, h, framebuf.RGB565)
+    for line in text.split('\n'):
+        fb.fill(background)
+        fb.text(line, 0, 0, color)
+        display.blit_buffer(buffer, x, y, w, h)
+        y += 8;
+        if y >= display.height:
+            break
 
 def delay_ms(ms):
     time.sleep_ms(ms)
