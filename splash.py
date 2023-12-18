9e5c31785b0e8b3cd9662ccae45d1593d3f15cbc
Respite09 <respite09@outlook.com>
2023-12-18 09:50:34 -0700

    Create splash.py

---
+++ splash.py
@@ -0,0 +1,15 @@
+import machine
+# import utime
+import st7789py
+import rgb_text
+
+
+espcolor = st7789py.BLUE
+spi = machine.SPI(1, baudrate=40000000, polarity=1)
+display = st7789py.ST7789(spi, 240, 240, reset=machine.Pin(5, machine.Pin.OUT), dc=machine.Pin(4, machine.Pin.OUT))
+display.init()
+
+rgb_text.text(display, 'Welcome to microOS', 10, 10)
+
+
+
